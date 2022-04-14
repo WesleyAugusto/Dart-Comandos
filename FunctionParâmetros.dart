66d021412edbafaacad185366efb3aee7d7d67bb
WesleyAugusto <ranzerap@gmail.com>
2022-04-14 13:22:03 -0300

    funcao anonima

--- FunctionParâmetros.dart
+++ FunctionParâmetros.dart
@@ -1,6 +1,6 @@
 //Colocar ? Na Frente Do Atribulto.
 //Chaves {} Para Deixa Parâmetros opcionais.
-
+/*
 void exibirDados(String nome, {int? idade, double? altura}) {
   print("nome  $nome");
   print("idade   $idade ");
@@ -8,5 +8,22 @@ void exibirDados(String nome, {int? idade, double? altura}) {
 }
 
 void main() {
-  exibirDados("Wesley");
+  exibirDados("Wesley", idade: 30, altura: 1.73);
+}
+*/
+
+void calcularBonus() {
+  print("seu bonus é de:200");
+}
+
+void calcularSalario(double salario, Function funcaoParametro) {
+  print("seu salario È :$salario");
+  funcaoParametro();
+}
+
+//(){} Funçao anônima,Codigos Customizado.
+void main() {
+  calcularSalario(100, () {
+    print("seu bonus é de:200");
+  });
 }
